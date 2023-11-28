5f420023838686dfc8096aed6efacf4f5eece11e
GarrettWilkerson <gwilkerson@firstorion.com>
2023-11-28 11:32:49 -0600

    working on aws eks all the way. using FQDN for jdbc url

--- todo-backend/src/main/kotlin/com/cornerstone/todo/todobackend/TodoBackendApplication.kt
+++ todo-backend/src/main/kotlin/com/cornerstone/todo/todobackend/TodoBackendApplication.kt
@@ -3,6 +3,7 @@ package com.cornerstone.todo.todobackend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+
 @SpringBootApplication
 class TodoBackendApplication
 
