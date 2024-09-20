bba8ba9045319d016b1aa3c88d8f48fc994842fb
gwilkerson <gwilkerson@firstorion.com>
2024-09-20 09:36:21 -0500

    remove sensitive outputs. add api gateway endpoint output

--- deployment/terraform/outputs.tf
+++ deployment/terraform/outputs.tf
@@ -1,12 +1,4 @@
 
-output "alb_url" {
-  value = "http://${aws_alb.action-items-alb.dns_name}"
-}
-
-output "db_instance_endpoint" {
-  value = aws_db_instance.action_items_postgres_db.endpoint
-}
-
-output "db_instance_address" {
-  value = aws_db_instance.action_items_postgres_db.address
+output "api_gateway_endpoint" {
+  value = aws_apigatewayv2_api.ActionITemsAPI.api_endpoint
 }
